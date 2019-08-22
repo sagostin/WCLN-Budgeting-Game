58ae9edae0f6948b925c0c983563b29256cba514
Shaun Agostinho <shaunagostinho@gmail.com>
2019-08-22 10:54:46 -0700

    first commit!

---
+++ js/game.js
@@ -0,0 +1,110 @@
+/**
+ * WCLN.ca
+ * Hockey Matchup
+ * @author Shaun Agostinho (shaunagostinho@gmail.com)
+ * July 2019
+ */
+
+let FPS = 24;
+let gameStarted = false;
+let STAGE_WIDTH, STAGE_HEIGHT;
+let stage = new createjs.Stage("gameCanvas"); // canvas id is gameCanvas
+
+// bitmap letiables
+let background
+
+/*
+ * Called by body onload
+ */
+function init() {
+    STAGE_WIDTH = parseInt(document.getElementById("gameCanvas").getAttribute("width"));
+    STAGE_HEIGHT = parseInt(document.getElementById("gameCanvas").getAttribute("height"));
+
+    // init state object
+    stage.mouseEventsEnabled = true;
+    stage.enableMouseOver(); // Default, checks the mouse 20 times/second for hovering cursor changes
+
+    setupManifest(); // preloadJS
+    startPreload();
+
+    gameStarted = false;
+
+    stage.update();
+}
+
+/*
+ * Displays the end game screen and score.
+ */
+function endGame() {
+    gameStarted = false;
+}
+
+function setupManifest() {
+    manifest = [
+        {
+            src: "img/bg.png",
+            id: "background"
+        }
+    ];
+}
+
+function startPreload() {
+    let preload = new createjs.LoadQueue(true);
+    preload.installPlugin(createjs.Sound);
+    preload.on("fileload", handleFileLoad);
+    preload.on("progress", handleFileProgress);
+    preload.on("complete", loadComplete);
+    preload.on("error", loadError);
+    preload.loadManifest(manifest);
+}
+
+// not currently used as load time is short
+function handleFileProgress(event) {
+    /*progressText.text = (preload.progress*100|0) + " % Loaded";
+    progressText.x = STAGE_WIDTH/2 - progressText.getMeasuredWidth() / 2;
+    stage.update();*/
+}
+
+function handleFileLoad(event) {
+    console.log("A file has loaded of type: " + event.item.type);
+    // create bitmaps of images
+    if (event.item.id == "background") {
+        background = new createjs.Bitmap(event.result);
+    }
+}
+
+function loadError(evt) {
+    console.log("Error!", evt.text);
+}
+
+/*
+ * Displays the start screen.
+ */
+function loadComplete(event) {
+    console.log("Finished Loading Assets");
+
+    createjs.Ticker.setFPS(FPS);
+    createjs.Ticker.addEventListener("tick", update); // call update function
+
+    stage.addChild(background);
+
+    initGraphics();
+}
+
+/**
+ * Load the basic stuff
+ *
+ */
+function initGraphics() {
+    gameStarted = true;
+}
+
+/**
+ * Update the stage. (Tween Ticker)
+ *
+ * @param event
+ */
+
+function update(event) {
+    stage.update(event);
+}
